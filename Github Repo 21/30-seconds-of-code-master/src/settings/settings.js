a90fca89e9c977e3e79c0869a1b1d898f2ae684f
Akshat B. Gupta <gakshatb@gmail.com>
2026-01-02 14:06:00 +0530

    Commit 456

---
+++ Github Repo 21/30-seconds-of-code-master/src/settings/settings.js
@@ -0,0 +1,13 @@
+import global from './global.js';
+import paths from './paths.js';
+import searchEngine from './searchEngine.js';
+import presentation from './presentation.js';
+import tags from './tags.js';
+
+export default {
+  global,
+  paths,
+  searchEngine,
+  presentation,
+  tags,
+};
---
+++ Github Repo 21/30-seconds-of-code-master/src/settings/tags.js
@@ -0,0 +1,11 @@
+export default {
+  css: 'CSS',
+  javascript: 'JavaScript',
+  node: 'Node.js',
+  php: 'PHP',
+  seo: 'SEO',
+  vscode: 'Visual Studio Code',
+  html: 'HTML',
+  webdev: 'Web development',
+  http: 'HTTP',
+};
